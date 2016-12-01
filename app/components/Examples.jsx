41ac68fa6bee735f5c15368b27ca21f2f9631e27
Brooks Garrett <brooks+git@brooksgarrett.com>
2016-11-30 19:42:33 -0500

    Lecture 60. ReactWeather is complete. Add Nav search.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -7,7 +7,7 @@ var Examples = (props) => {
                 <h1 className="text-center page-title">Examples</h1>
                 <p>Here are some sample locations to try out:</p>
                 <ol>
-                    <li><Link to='/?location=Philadelphia'>Philadelphia, PA</Link></li>
+                    <li><Link to='/?location=Philadelphia%20PA'>Philadelphia, PA</Link></li>
                     <li><Link to='/?location=Atlanta'>Atlanta, GA</Link></li>
                 </ol>
             </div>
